5028e6df22ef63d5f3831e571fe2895169cd997e
kimhongjung <hong267@naver.com>
2022-03-09 16:06:41 +0900

    F00-1_Entity : entity remainder create

--- backend/src/main/java/com/ssafy/rikey/db/entity/BikeRoad.java
+++ backend/src/main/java/com/ssafy/rikey/db/entity/BikeRoad.java
@@ -1,4 +1,36 @@
 package com.ssafy.rikey.db.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Table(name = "bike_road")
+@NoArgsConstructor
 public class BikeRoad {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "bike_road_id")
+    private Long id;
+
+    private String course;
+
+    private String name;
+
+    private double departureLatitude;
+
+    private double departureLongitude;
+
+    private double arrivalLatitude;
+
+    private double arrivalLongitude;
+
+    private int hour;
+
+    private int minute;
+
+    private String introduce;
 }
--- backend/src/main/java/com/ssafy/rikey/db/entity/Toilet.java
+++ backend/src/main/java/com/ssafy/rikey/db/entity/Toilet.java
@@ -1,4 +1,30 @@
 package com.ssafy.rikey.db.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Table(name = "toilet")
+@NoArgsConstructor
 public class Toilet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "toilet_id")
+    private Long id;
+
+    private String name;
+
+    private String roadAddress;
+
+    private String address;
+
+    private String openTime;
+
+    private double latitude;
+
+    private double longitude;
 }
