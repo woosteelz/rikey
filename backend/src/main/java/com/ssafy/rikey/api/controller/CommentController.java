38c7c720071734056353b0a5338ec1f8f8293a03
khyunchoi <khyunchoi@naver.com>
2022-03-09 15:36:26 +0900

    F03-10_댓글_삭제 : 댓글 삭제 API 추가

--- backend/src/main/java/com/ssafy/rikey/api/controller/CommentController.java
+++ backend/src/main/java/com/ssafy/rikey/api/controller/CommentController.java
@@ -1,12 +1,13 @@
 package com.ssafy.rikey.api.controller;
 
 import com.ssafy.rikey.api.request.CreateCommentRequestDto;
+import com.ssafy.rikey.api.service.ArticleService;
 import com.ssafy.rikey.api.service.CommentService;
+import com.ssafy.rikey.db.entity.Article;
 import com.ssafy.rikey.db.entity.Comment;
 import com.ssafy.rikey.db.repository.CommentRepository;
 import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,9 +25,11 @@ import java.util.NoSuchElementException;
 @RequiredArgsConstructor
 public class CommentController {
 
+    private ArticleService articleService;
     private CommentService commentService;
     private CommentRepository commentRepository;
 
+    // 댓글 등록
     @PostMapping
     @ApiOperation(value = "댓글 등록", notes = "새로운 댓글을 등록한다.")
     @ApiResponses({
@@ -48,7 +51,8 @@ public class CommentController {
         }
     }
 
-    @PutMapping
+    // 댓글 수정
+    @PutMapping("/{articleId}")
     @ApiOperation(value = "댓글 수정", notes = "댓글을 수정한다.")
     @ApiResponses({
             @ApiResponse(code = 200, message = "성공(SUCCESS)"),
@@ -64,6 +68,28 @@ public class CommentController {
 
             // 유저 확인 로직 필요
             commentService.updateComment(commentInfo, commentId, articleId);
+            return new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<String>("NO ARTICLE", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    // 댓글 삭제
+    @DeleteMapping("/{articleId}")
+    @ApiOperation(value = "댓글 삭제", notes = "댓글을 삭제한다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공(SUCCESS)"),
+            @ApiResponse(code = 400, message = "게시글 탐색 오류(NO ARTICLE)"),
+    })
+    public ResponseEntity<String> deleteComment(
+            @PathVariable("articleId") @ApiParam(value="게시글 id", required = true) Long articleId,
+            @PathVariable("commentId") @ApiParam(value="댓글 id", required = true) Long commentId) {
+
+        try {
+            Comment comment = commentRepository.findByIdAndArticleId(commentId, articleId).get();
+
+            // 유저 확인 로직 필요
+            commentService.deleteComment(comment);
             return new ResponseEntity<String>("SUCCESS", HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<String>("NO ARTICLE", HttpStatus.BAD_REQUEST);
