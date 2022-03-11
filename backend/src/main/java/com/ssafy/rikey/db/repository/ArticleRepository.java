2f32d90cf1cf8d49630d9c796ac93c1c76044851
yeongsummer <qwer62636275@gmail.com>
2022-03-11 11:53:22 +0900

    F03게시글:수정

--- backend/src/main/java/com/ssafy/rikey/db/repository/ArticleRepository.java
+++ backend/src/main/java/com/ssafy/rikey/db/repository/ArticleRepository.java
@@ -1,16 +1,18 @@
 package com.ssafy.rikey.db.repository;
 
 import com.ssafy.rikey.api.response.ArticleResponseDto;
+import com.ssafy.rikey.db.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.ssafy.rikey.db.entity.Article;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
-    List<Article> findTop3ByCategoryOrderByHitsDesc();
+    List<Article> findTop3ByCategoryOrderByHitsDesc(Category category);
     List<Article> findTop3ByOrderByHitsDesc();
-    List<Article> findAllOrderByIdDesc();
-    List<Article> findByCategoryOrderByIdDesc(String category);
+    List<Article> findAllByOrderByIdDesc();
+    List<Article> findByCategoryOrderByIdDesc(Category category);
     List<Article> findTop5ByOrderByIdDesc();
 }
