1e27d62c346ae70c57bc0c27a142ba174164acd0
KimHyeongJun <hjthelegend@naver.com>
2022-03-11 15:02:50 +0900

    F02-2_로그인_ : (네이버 소셜 로그인으로 변경, 로그인 및 로그아웃 기능)

--- frontend/index.js
+++ frontend/index.js
@@ -4,7 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
-import FirstPage from './screens/FirstPage'
+import SigninPage from './screens/SigninPage'
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
