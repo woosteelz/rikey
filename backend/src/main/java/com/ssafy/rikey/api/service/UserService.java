a99220a046fbb70eca163a420d7ca39948f6078c
yeongsummer <qwer62636275@gmail.com>
2022-03-23 10:20:26 +0900

    nginx: 설정 2번째

--- backend/src/main/java/com/ssafy/rikey/api/service/UserService.java
+++ backend/src/main/java/com/ssafy/rikey/api/service/UserService.java
@@ -5,7 +5,10 @@ import com.ssafy.rikey.api.request.UserRequestDto;
 import com.ssafy.rikey.api.response.UserRankingResponseDto;
 import com.ssafy.rikey.api.response.UserResponseDto;
 import com.ssafy.rikey.api.response.UserSimpleResponseDto;
+import org.apache.tomcat.util.http.fileupload.FileUploadException;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface UserService {
@@ -16,4 +19,5 @@ public interface UserService {
     UserResponseDto getUserProfile(String nickName);
     void deleteUser(String userId);
     List<Integer> getRankings(String nickname, String area);
+    String uploadImage(MultipartFile multipartFile) throws Exception;
 }
