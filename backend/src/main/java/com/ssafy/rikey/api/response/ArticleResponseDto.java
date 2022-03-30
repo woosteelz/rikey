3c1390fdd990af90a95a3e7827412019bfeb7b70
yeongsummer <qwer62636275@gmail.com>
2022-03-30 22:53:58 +0900

    F03:내게시글조회API수정

--- backend/src/main/java/com/ssafy/rikey/api/response/ArticleResponseDto.java
+++ backend/src/main/java/com/ssafy/rikey/api/response/ArticleResponseDto.java
@@ -6,6 +6,8 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @NoArgsConstructor
 @ApiModel("ArticleResponseDto")
@@ -23,10 +25,14 @@ public class ArticleResponseDto {
     @ApiModelProperty(value = "게시글 작성자 닉네임", example = "영하")
     private String author;
 
+    @ApiModelProperty(value = "게시글 작성일", example = "2022-02-01 23:59:59.500")
+    private LocalDateTime createdTime;
+
     public ArticleResponseDto(Article article){
         articleId = article.getId();
         title = article.getTitle();
         content = article.getContent();
         author = article.getAuthor().getNickName();
+        createdTime = article.getCreatedTime();
     }
 }
