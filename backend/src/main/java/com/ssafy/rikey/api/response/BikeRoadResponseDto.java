a93250259ef47635aefbe61211cca1330b1e4218
yeongsummer <qwer62636275@gmail.com>
2022-03-30 14:04:15 +0900

    F05:코스이미지추가

--- backend/src/main/java/com/ssafy/rikey/api/response/BikeRoadResponseDto.java
+++ backend/src/main/java/com/ssafy/rikey/api/response/BikeRoadResponseDto.java
@@ -23,6 +23,9 @@ public class BikeRoadResponseDto {
     @ApiModelProperty(value = "리뷰 점수", example = "3.3")
     private float score;
 
+    @ApiModelProperty(value = "코스 이미지", example = "http://j6c208.p.ssafy.io/images/course/1.jpg")
+    private String image;
+
     public BikeRoadResponseDto(BikeRoad bikeRoad) {
         bikeroadId = bikeRoad.getId();
         name = bikeRoad.getName();
